71e0a003ccba3048492a3453b40e823caa3cdd62
MasterYoav <realyoavperetz@gmail.com>
2025-09-22 13:20:13 +0300

    adding readme

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
-// jest.config.js
 module.exports = {
-    preset: 'jest-expo',
-    testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-    transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
-    transformIgnorePatterns: [
-      'node_modules/(?!(jest-)?react-native|expo(nent)?|@expo(nent)?/.*|@react-native/.*)',
-    ],
-    setupFiles: ['<rootDir>/jest.setup.ts'],
-    collectCoverageFrom: [
-      'src/**/*.{ts,tsx}',
-      '!src/**/__mocks__/**',
-      '!src/**/supabase.ts', // if you don’t want to count this
-    ],
-  };
+  preset: 'jest-expo',
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|expo(nent)?|@expo(nent)?/.*|@react-native/.*)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/__mocks__/**',
+    '!src/**/supabase.ts',
+  ],
+  reporters: process.env.CI
+    ? ['default', ['jest-junit', { outputDirectory: './coverage', outputName: 'junit.xml' }]]
+    : undefined,
+};
